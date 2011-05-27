c8026f3911b9bbf62c022182b393ce6f2b924f0f
tom atwood <tom@cloudsuitemedia.com>
2011-05-26 18:21:40 -0700

    changed README

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Tmror::Application.routes.draw do
   
   get "home/index"
   
-  root :to => "home#index"
+  root :to => "home#index", :as => "index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
